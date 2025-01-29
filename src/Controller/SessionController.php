f6599f5164dcc942708623d1b16c859a817dd88b
LEDLEV <estherlvn@gmail.com>
2025-01-29 14:04:34 +0100

    lister les formations

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(SessionRepository $sessionRepository): Response
     {
+
+        $sessions = $sessionRepository->findBy([], ['dateDebut'=>'ASC']);
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
         ]);
     }
 }
