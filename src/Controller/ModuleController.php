a92aedf83da598f3c762d93d0ebc4d0dafb19891
Estherlvn <estherlvn@gmail.com>
2025-02-04 00:20:57 +0100

    ajouter un module

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Module;
+use App\Form\ModuleType;
 use App\Repository\ModuleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,5 +23,31 @@ final class ModuleController extends AbstractController
         ]);
     }
 
-    
+
+    #[Route('/new', name: 'new_module')]
+    #[Route('/{id}/edit', name: 'edit_module', requirements: ['id' => '\d+'])]
+    public function new_module(
+        ?int $id,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ModuleRepository $moduleRepository
+        ): Response {
+        // Trouver le module s'il existe, sinon en créer un nouveau
+        $module = $id ? $moduleRepository->find($id) : new Module();
+
+        $form = $this->createForm(ModuleType::class, $module);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($module);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_module');
+        }
+
+        return $this->render('module/new.html.twig', [
+            'formAddModule' => $form,
+        ]);
+    }
 }
+
