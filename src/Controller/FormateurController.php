c37f1beb9b8e082ee9414837a5f0b332dda12740
Estherlvn <estherlvn@gmail.com>
2025-02-03 17:46:34 +0100

    ajouter un nv formateur/ ajouter un nv stagiaire

--- src/Controller/FormateurController.php
+++ src/Controller/FormateurController.php
@@ -2,23 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Formateur;
+use App\Form\FormateurType;
 use App\Repository\FormateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class FormateurController extends AbstractController
 {
-    #[Route('/formateur', name: 'app_formateur')]
 
-    public function index(FormateurRepository $formateurRepository): Response
+        // Afficher la liste des formateurs de la bdd avec findby
+        #[Route('/formateur', name: 'app_formateur')]
 
-    {
+        public function index(FormateurRepository $formateurRepository): Response
 
-        $formateurs = $formateurRepository->findBy([], ['nom'=>'ASC']); 
-        return $this->render('formateur/index.html.twig', [
-            'formateurs' => $formateurs
+        {
+            $formateurs = $formateurRepository->findBy([], ['nom'=>'ASC']); 
+            return $this->render('formateur/index.html.twig', [
+                'formateurs' => $formateurs
 
-        ]);
-    }
+            ]);
+        }
+
+    
+        // Créer un nouveau formateur ou modifier un formateur existant
+        #[Route('/formateur/new', name: 'new_formateur')]
+        #[Route('/formateur/{id}/edit', name: 'edit_formateur')]
+        public function new_formateur(Formateur $formateur = null, Request $request, EntityManagerInterface $entityManager): Response
+        {
+
+            if(!$formateur) { 
+                $formateur = new Formateur(); // Crée un nouveau formateur
+            }
+
+            $form = $this->createForm(FormateurType::class, $formateur); // Associe le formulaire du formateur
+
+            // Soumission du formulaire et insertion en bdd
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $formateur = $form->getData();
+
+                $entityManager->persist($formateur); // persist (= prepare, en PDO)
+                // Exécute les requêtes (INSERT)
+                $entityManager->flush(); // flush: envoyer en bdd (= execute)
+
+                return $this->redirectToRoute('app_formateur'); // Redirection vers la liste des formateurs
+            }
+
+            return $this->render('formateur/new.html.twig', [
+                'formAddFormateur' => $form, // Le formulaire sera envoyé à la vue
+            ]);
+        }
+
+
 }
--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class StagiaireController extends AbstractController
 {
+
+    // Afficher la liste des stagiaires de la bdd avec findby
     #[Route('/stagiaire', name: 'app_stagiaire')]
     public function index(StagiaireRepository $stagiaireRepository): Response
     {
@@ -18,4 +24,38 @@ final class StagiaireController extends AbstractController
             'stagiaires' => $stagiaires,
         ]);
     }
+
+
+    // Créer un nouveau stagiaire ou modifier un stagiaire existant
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function new_stagiaire(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        if(!$stagiaire) { 
+            $stagiaire = new Stagiaire();
+        }
+
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+
+        // soumisson du formulaire et insertion en bdd
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $stagiaire = $form->getData();
+
+            $entityManager->persist($stagiaire); // persist (= prepare, en PDO)
+            // actually executes the queries (INSERT query)
+            $entityManager->flush(); // flush: envoyer en  bdd (= execute, en PDO)
+
+
+    return $this->redirectToRoute('app_stagiaire');
+    }
+
+    return $this->render('stagiaire/new.html.twig', [
+        'formAddStagiaire' => $form,
+    ]);
+    }
+
 }
