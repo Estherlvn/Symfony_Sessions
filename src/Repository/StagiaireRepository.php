23319a3991fd5ef9df87841e2d20c0acd37b8122
Estherlvn <estherlvn@gmail.com>
2025-02-05 09:29:05 +0100

    requete DQL : afficher stagiaires non inscrits

--- src/Repository/StagiaireRepository.php
+++ src/Repository/StagiaireRepository.php
@@ -17,26 +17,58 @@ class StagiaireRepository extends ServiceEntityRepository
         parent::__construct($registry, Stagiaire::class);
     }
 
-    // Récupérer tous les stagiaires non-inscrits dans une formation donnée
-        public function findStagiairesNonInscrits(int $sessionId): array
-    {
-        $conn = $this->getEntityManager()->getConnection();
+    // // Récupérer tous les stagiaires non-inscrits dans une session donnée SQL
+    //     public function findStagiairesNonInscrits(int $sessionId): array
+    // {
+    //     $conn = $this->getEntityManager()->getConnection();
 
-        $sql = '
-            SELECT * FROM stagiaire s
-            WHERE s.id NOT IN (
-                SELECT stagiaire_id FROM session_stagiaire WHERE session_id = :sessionId
-            )
-        ';
+    //     $sql = '
+    //         SELECT * FROM stagiaire s
+    //         WHERE s.id NOT IN (
+    //             SELECT stagiaire_id FROM session_stagiaire WHERE session_id = :sessionId
+    //         )
+    //     ';
 
-        try {
-            $stmt = $conn->prepare($sql);
-            $resultSet = $stmt->executeQuery(['sessionId' => $sessionId]);
-            return $resultSet->fetchAllAssociative();
-        } catch (\Exception $e) {
-            return [];
-        }
-    }
+    //     try {
+    //         $stmt = $conn->prepare($sql);
+    //         $resultSet = $stmt->executeQuery(['sessionId' => $sessionId]);
+    //         return $resultSet->fetchAllAssociative();
+    //     } catch (\Exception $e) {
+    //         return [];
+    //     }
+    // }
+
+     // Récupérer tous les stagiaires non-inscrits dans une session donnée DQL
+     public function findStagiairesNonInscrits($session_id)
+     {
+        $em = $this->getEntityManager();
+        $sub = $em->createQueryBuilder();
+
+        $qb = $sub;
+        // sélectionner tous les stagiaires d'une session dont l'id est passé en paramètre
+        $qb->select('s')
+            ->from('App\Entity\Stagiaire', 's')
+            ->leftJoin('s.sessions', 'se')
+            ->where('se.id = :id');
+
+        $sub = $em->createQueryBuilder();
+        // sélectionner tous les stagiaires qui NE SONT PAS (NOT IN) dans le résultat précédent
+        // on obtient donc les stagiares non-inscrits pour une session définie
+        $sub->select('st')
+            ->from('App\Entity\Stagiaire', 'st')
+            ->where($sub->expr()->notIn('st.id', $qb->getDQL()))
+            // requête paramétrée
+            ->setParameter('id', $session_id)
+            // trier la liste des stagiaires sur le nom de famille
+            -> orderBy('st.nom');
+
+        // renvoyer le résultat
+        $query = $sub->getQuery();
+        return $query->getResult();
+     }
+
+
+
 
 
     //    /**
